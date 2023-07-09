9549d69b87b631d373c89c58cfe64973ec5352f2
user7889 <user7889810@gmail.com>
2023-07-09 15:15:29 +0530

    first code committing

---
+++ src/main/java/checkPractise/HelloJava.java
@@ -0,0 +1,12 @@
+package checkPractise;
+
+public class HelloJava {
+
+	public static void main(String[] args) {
+
+		System.out.println("Hello Java I am using you first you");
+                int i = 10;
+
+	}
+
+}
